cff1e2fbf178f6024c54593fc42b9246bcdcef94
Pujitha <pujithakothuri713@gmail.com>
2026-06-24 01:18:07 +0530

    Completed Exercise 2

---
+++ Week-1/Algorithms_Data_Structure/Exercise-2/LinearSearch.java
@@ -0,0 +1,16 @@
+class LinearSearch {
+
+    public static Product search(
+            Product[] products,
+            int targetId) {
+
+        for(Product product : products) {
+
+            if(product.productId == targetId) {
+                return product;
+            }
+        }
+
+        return null;
+    }
+}
